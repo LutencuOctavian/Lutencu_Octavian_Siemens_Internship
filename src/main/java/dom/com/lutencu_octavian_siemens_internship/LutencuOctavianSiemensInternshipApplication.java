c5541932136626a0d97db9ee95addc49d2ec9615
Octav <octavv2001@yahoo.com>
2024-05-15 13:04:37 +0300

    first commit, default settings of project: JPA + MySQL connector + WEB Spring

---
+++ src/main/java/dom/com/lutencu_octavian_siemens_internship/LutencuOctavianSiemensInternshipApplication.java
@@ -0,0 +1,13 @@
+package dom.com.lutencu_octavian_siemens_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutencuOctavianSiemensInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LutencuOctavianSiemensInternshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dom/com/lutencu_octavian_siemens_internship/LutencuOctavianSiemensInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package dom.com.lutencu_octavian_siemens_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LutencuOctavianSiemensInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
